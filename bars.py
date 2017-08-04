1ead188183c171515301a2c52d4fd143f707b96a
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-05 00:32:15 +0300

    Правки

--- bars.py
+++ bars.py
@@ -1,20 +1,22 @@
 import json
 import os
 import argparse
-import pprint
 
 def load_data(filepath):
     if not os.path.isfile(filepath):
         return None
     with open(filepath, 'r', encoding = 'cp1251') as raw_json_file:
-        raw_json_file_text = json.load(raw_json_file)
-        #return raw_json_file_text
-        print(raw_json_file_text)
+        deserialized_json = json.load(raw_json_file)
+        #return deserialized_json
+        print(deserialized_json)
 
-
-def get_biggest_bar(data):
-    pass
-
+def get_biggest_bar(bars_data):
+    max_seats_number = 0
+    biggest_bar_name = str()
+    for bar_info in load_data(bars_data):
+        if bar_info.get('SeatsCount') > max_seats_number:
+            max_seats_number = bar_info.get('SeatsCount')
+    print(max_seats_number)
 
 def get_smallest_bar(data):
     pass
@@ -29,6 +31,6 @@ if __name__ == '__main__':
     parser.add_argument('-p','--path', required = True,
                         help = 'Enter filepath file')
     namespace = parser.parse_args()
-    print(namespace)
+    #print(namespace)
     if namespace.path:
         load_data(namespace.path)
