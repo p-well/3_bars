9de25c516c51c461c1370d1e0d6713e03d28808e
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-06 17:55:50 +0300

    Add searth for the nearest bar

--- bars.py
+++ bars.py
@@ -12,34 +12,45 @@ def load_data(filepath):
 
 def get_biggest_bar(bars_data):
     biggest_bar_data = max(bars_data, key = lambda bars: bars.get('SeatsCount'))
-    #print(biggest_bar_data)
+
     print('Самый большой бар в Москве - это "{}". В нем {} мест и находится он\nпо адресу: {}.'.
         format(biggest_bar_data.get('Name'),
                biggest_bar_data.get('SeatsCount'),
                biggest_bar_data.get('Address')
                )
         )
-   
 
 def get_smallest_bar(bars_data):
     smallest_bar_data = min(bars_data, key = lambda bars: bars.get('SeatsCount'))
-    print('Самый маленький бар в Москве - это "{}". В нем {} мест и находится он\nпо адресу: {}.'.
+
+    print('А самый маленький бар в Москве - это "{}". В нем {} мест и находится он\nпо адресу: {}.'.
         format(smallest_bar_data.get('Name'),
                smallest_bar_data.get('SeatsCount'),
                smallest_bar_data.get('Address')
                )
         )
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bars_data, longitude, latitude):
+    x_user = longitude
+    y_user = latitude
+    for bar_data in bars_data:
+        x_bar = bar_data.get('Longitude_WGS84')
+        y_bar = bar_data.get('Latitude_WGS84')
+        distance = sqrt((x_user - x_bar)**2 + (y_user - y_bar)**2)
+        bar_data['User_distance'] = distance
+    closest_bar_data = min(bars_data, key = lambda bar: bar.get('User_distance'))
+    print('Ближайший к вам бар - это {}. Его адрес {}'.
+        format(closest_bar_data.get('Name'), closest_bar_data.get('Address')))    
 
 
 if __name__ == '__main__':
+    #load_data('raw_json.json')
     moscow_bars = load_data('raw_json.json')
     get_biggest_bar(moscow_bars) 
-
     get_smallest_bar(moscow_bars)
-
+    user_longitude = float(input("Enter your longitude:"))
+    user_latitude = float(input("Enter your latitude:"))
+    get_closest_bar(moscow_bars, user_longitude, user_latitude)
 
     # parser = argparse.ArgumentParser()
     # parser.add_argument('-p','--path', required = True,
